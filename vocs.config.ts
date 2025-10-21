5369dc5838359af0ab9806ccb7dc1b66475e981a
Sara Russo <sararusso984@gmail.com>
2025-10-21 18:12:21 +0200

    Update vocs.config.ts
    Signed-off-by: Sara Russo <sararusso984@gmail.com>

--- vocs.config.ts
+++ vocs.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vocs'
 
 const config = {
   ogImageUrl: {
-    
     '/': 'https://frameworks-static.s3.us-east-2.amazonaws.com/images/safe-harbor/legal.png',
+    '/safe-harbor/': 'https://frameworks-static.s3.us-east-2.amazonaws.com/images/safe-harbor/Uniswap_logo.png',
   },
   banner: {
     content: '***This is a work in progress and not a release. We are looking for volunteers. See [Issues](https://github.com/security-alliance/frameworks/issues) and [Contribution](https://github.com/security-alliance/frameworks/blob/develop/docs/pages/contribute/contributing.mdx) to know how to collaborate.***',
